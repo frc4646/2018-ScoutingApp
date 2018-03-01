15c63261ae3de2f0e22544402612b97230089d04
Alexander Young <meun5@users.noreply.github.com>
2018-03-01 02:32:57 -0600

    Rework wording

--- ScoutingAppWeb/src/pages/results.js
+++ ScoutingAppWeb/src/pages/results.js
@@ -292,6 +292,15 @@ class ResultSpecific extends Component {
               <Typography type="display1" gutterBottom>
                 Overview
               </Typography>
+              <Typography type="caption" gutterBottom>
+                <span style={{
+                  marginRight: '2rem'
+                }}>T# = Total Number</span>
+                <span style={{
+                  marginRight: '2rem'
+                }}>{'{Ability Type}: {Average Choice} - X / # of Matches they did that.'}</span>
+                <span>{'T# of {Ability Type}: Total Number across all matches they played.'}</span>
+              </Typography>
               <div>
                 <Typography type="title">
                   Auto
@@ -302,22 +311,22 @@ class ResultSpecific extends Component {
                 <Grid container>
                   <Grid item xs={3}>
                     <Typography type="body1">
-                      Auto Cross: {stringToBoolean(collate['autoCross_Count'][0]) ? 'Yes' : 'No'} - {collate['autoCross_Count'][1]} out of {_.size(collate['autoCross'])} times
+                      Auto Cross: {stringToBoolean(collate['autoCross_Count'][0]) ? 'Yes' : 'No'} - {collate['autoCross_Count'][1]} / {_.size(collate['autoCross'])} 
                     </Typography>
                   </Grid>
                   <Grid item xs={3}>
                     <Typography type="body1">
-                      Auto Cube Pickup: {stringToBoolean(collate['autoCubePickup_Count'][0]) ? 'Yes' : 'No'} - {collate['autoCubePickup_Count'][1]} out of {_.size(collate['autoCubePickup'])} times
+                      Auto Cube Pickup: {stringToBoolean(collate['autoCubePickup_Count'][0]) ? 'Yes' : 'No'} - {collate['autoCubePickup_Count'][1]} / {_.size(collate['autoCubePickup'])} 
                     </Typography>
                   </Grid>
                   <Grid item xs={3}>
                     <Typography type="body1">
-                      Auto Cube Pickup Location: {_.capitalize(collate['autoCubePickupLocation_Count'][0]) || 'undefined'} - {collate['autoCubePickupLocation_Count'][1]} out of {_.size(collate['autoCubePickupLocation'])} times
+                      Auto Cube Pickup Location: {_.capitalize(collate['autoCubePickupLocation_Count'][0]) || 'undefined'} - {collate['autoCubePickupLocation_Count'][1]} / {_.size(collate['autoCubePickupLocation'])} 
                     </Typography>
                   </Grid>
                   <Grid item xs={3}>
                     <Typography type="body1">
-                      Auto Cube on Switch: {stringToBoolean(collate['autoSwitchCube_Count'][0]) ? 'Yes' : 'No'} - {collate['autoSwitchCube_Count'][1]} out of {_.size(collate['autoSwitchCube'])} times
+                      Auto Cube on Switch: {stringToBoolean(collate['autoSwitchCube_Count'][0]) ? 'Yes' : 'No'} - {collate['autoSwitchCube_Count'][1]} / {_.size(collate['autoSwitchCube'])} 
                     </Typography>
                   </Grid>
                   <Grid item xs={3}>
@@ -327,7 +336,7 @@ class ResultSpecific extends Component {
                   </Grid>
                   <Grid item xs={3}>
                     <Typography type="body1">
-                      Auto Cube on Wrong Side: {stringToBoolean(collate['autoCubeWrong_Count'][0]) ? 'Yes' : 'No'} - {collate['autoCubeWrong_Count'][1]} out of {_.size(collate['autoCubeWrong'])} times
+                      Auto Cube on Wrong Side: {stringToBoolean(collate['autoCubeWrong_Count'][0]) ? 'Yes' : 'No'} - {collate['autoCubeWrong_Count'][1]} / {_.size(collate['autoCubeWrong'])} 
                     </Typography>
                   </Grid>
                   <Grid item xs={3}>
@@ -337,51 +346,87 @@ class ResultSpecific extends Component {
                   </Grid>
                   <Grid item xs={3}>
                     <Typography type="body1">
-                      Starting Position: {_.capitalize(collate['startingPosition_Count'][0])} - {collate['startingPosition_Count'][1]} out of {_.size(collate['startingPosition'])} times
+                      Starting Position: {_.capitalize(collate['startingPosition_Count'][0])} - {collate['startingPosition_Count'][1]} / {_.size(collate['startingPosition'])} 
                     </Typography>
                   </Grid>
-                  {/* {_.map(_.pickBy(collate, (p, key) => !(_.endsWith(key, '_Count') || _.endsWith(key, 'Count'))), (arg1, arg2) => {
-                    console.log(arg1, arg2, collate[`${arg2}_Count`], 'nise');
-                    return ( 
-                      <Grid item xs={4}>
-                        <Typography type="headline">
-                          {arg2}: They did {collate[`${arg2}_Count`][0]} - {collate[`${arg2}_Count`][1]} out of {_.size(arg1)} times
-                        </Typography>
-                      </Grid>
-                    );
-                  })} */}
                 </Grid>
               </div>
+              <div>
+                <Typography type="title">
+                  Teleop
+                </Typography>
+                <Divider style={{
+                  marginBottom: '.5rem'
+                }} />
+                <Grid container>
+                  
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      Put Cube on Our Switch: {stringToBoolean(collate['teleopAllianceSwitchCube_Count'][0]) ? 'Yes' : 'No'} - {collate['teleopAllianceSwitchCube_Count'][1]} / {_.size(collate['teleopAllianceSwitchCube'])} 
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      T# of Cube on Our Switch: {collate['teleopAllianceSwitchCubeCount_Count'][0]}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      Put Cube on Opponent Switch: {stringToBoolean(collate['teleopOpponentSwitchCube_Count'][0]) ? 'Yes' : 'No'} - {collate['teleopOpponentSwitchCube_Count'][1]} / {_.size(collate['teleopOpponentSwitchCube'])} 
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      T# of Cube on Opponent Switch: {collate['teleopOpponentSwitchCubeCount_Count'][0]}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      Put Cube on Scale: {stringToBoolean(collate['teleopScaleCube_Count'][0]) ? 'Yes' : 'No'} - {collate['teleopScaleCube_Count'][1]} / {_.size(collate['teleopScaleCube'])} 
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      T# of Cubes on Scale: {collate['teleopScaleCubeCount_Count'][0]}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      Cube on Wrong Side of Our Switch: {stringToBoolean(collate['teleopAllianceSwitchCubeWrong_Count'][0]) ? 'Yes' : 'No'} - {collate['teleopAllianceSwitchCubeWrong_Count'][1]} / {_.size(collate['teleopAllianceSwitchCubeWrong'])} 
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      T# of Cubes on Wrong Side of Our Switch: {collate['teleopAllianceSwitchCubeWrongCount_Count'][0]}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      Cube on Wrong Side of Opp Switch: {stringToBoolean(collate['teleopOpponentSwitchCubeWrong_Count'][0]) ? 'Yes' : 'No'} - {collate['teleopOpponentSwitchCubeWrong_Count'][1]} / {_.size(collate['teleopOpponentSwitchCubeWrong'])} 
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      T# of Cubes on Wrong Side of Opp Switch: {collate['teleopOpponentSwitchCubeWrongCount_Count'][0]}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      Cube on Wrong Side of Scale: {stringToBoolean(collate['teleopAllianceSwitchCubeWrong_Count'][0]) ? 'Yes' : 'No'} - {collate['teleopAllianceSwitchCubeWrong_Count'][1]} / {_.size(collate['teleopAllianceSwitchCubeWrong'])} 
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      T# of Cubes on Wrong Side of Scale: {collate['teleopAllianceSwitchCubeWrongCount_Count'][0]}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography type="body1">
+                      Played Defense: {stringToBoolean(collate['teleopDefense_Count'][0]) ? 'Yes' : 'No'} - {collate['teleopDefense_Count'][1]} / {_.size(collate['teleopDefense'])} 
+                    </Typography>
+                  </Grid>
                 </Grid>
-                <Grid item xs={3}>
-                  <Typography type="body1">
-                    Auto Cube Pickup Location: {_.capitalize(collate['autoCubePickupLocation_Count'][0]) || 'undefined'} - {collate['autoCubePickupLocation_Count'][1]} out of {_.size(collate['autoCubePickupLocation'])} times
-                  </Typography>
-                </Grid>
-                <Grid item xs={3}>
-                  <Typography type="body1">
-                    Auto Cube on Switch: {stringToBoolean(collate['autoSwitchCube_Count'][0]) ? 'Yes' : 'No'} - {collate['autoSwitchCube_Count'][1]} out of {_.size(collate['autoSwitchCube'])} times
-                  </Typography>
-                </Grid>
-                <Grid item xs={3}>
-                  <Typography type="body1">
-                    T# of Auto Cube on Switch: {collate['autoSwitchCubeCount_Count'][0]}
-                  </Typography>
-                </Grid>
-                <Grid item xs={3}>
-                  <Typography type="body1">
-                    Auto Cube on Wrong Side: {stringToBoolean(collate['autoCubeWrong_Count'][0]) ? 'Yes' : 'No'} - {collate['autoCubeWrong_Count'][1]} out of {_.size(collate['autoCubeWrong'])} times
-                  </Typography>
-                </Grid>
-                <Grid item xs={3}>
-                  <Typography type="body1">
-                    T# of Auto Cube on Wrong Side: {collate['autoCubeWrongCount_Count'][0]}
-                  </Typography>
-                </Grid>
-                <Grid item xs={3}>
-                  <Typography type="body1">
-                    Starting Position: {_.capitalize(collate['startingPosition_Count'][0])} - {collate['startingPosition_Count'][1]} out of {_.size(collate['startingPosition'])} times
-                  </Typography>
+              </div>
               <div>
                 <Typography type="title">
                   Powerups
@@ -392,17 +437,17 @@ class ResultSpecific extends Component {
                 <Grid container>
                   <Grid item xs={3}>
                     <Typography type="body1">
-                      Played Boost: {stringToBoolean(collate['powerupBoost_Count'][0]) ? 'Yes' : 'No'} - {collate['powerupBoost_Count'][1]} out of {_.size(collate['powerupBoost'])} times - Avg: {_.mean(collate['powerupBoostCount'])}
+                      Played Boost: {stringToBoolean(collate['powerupBoost_Count'][0]) ? 'Yes' : 'No'} - {collate['powerupBoost_Count'][1]} / {_.size(collate['powerupBoost'])} - Avg #: {_.mean(collate['powerupBoostCount'])}
                     </Typography>
                   </Grid>
                   <Grid item xs={3}>
                     <Typography type="body1">
-                      Played Force: {stringToBoolean(collate['powerupForce_Count'][0]) ? 'Yes' : 'No'} - {collate['powerupForce_Count'][1]} out of {_.size(collate['powerupForce'])} times - Avg: {_.mean(collate['powerupForceCount'])}
+                      Played Force: {stringToBoolean(collate['powerupForce_Count'][0]) ? 'Yes' : 'No'} - {collate['powerupForce_Count'][1]} / {_.size(collate['powerupForce'])} - Avg #: {_.mean(collate['powerupForceCount'])}
                     </Typography>
                   </Grid>
                   <Grid item xs={3}>
                     <Typography type="body1">
-                      Played Levitate: {stringToBoolean(collate['teleopDefense_Count'][0]) ? 'Yes' : 'No'} - {collate['teleopDefense_Count'][1]} out of {_.size(collate['teleopDefense'])} times
+                      Played Levitate: {stringToBoolean(collate['powerupLevitate_Count'][0]) ? 'Yes' : 'No'} - {collate['powerupLevitate_Count'][1]} / {_.size(collate['powerupLevitate'])} 
                     </Typography>
                   </Grid>
                 </Grid>
